6a76cd8f8bd1f1a4d4dbe33530556550b3f97a21
Juhi Bajpai <juhi.altid@gmail.com>
2024-06-27 19:30:40 +0530

    Update BookingCard.js

--- src/component/AppointMent/BookingCard/BookingCard.js
+++ src/component/AppointMent/BookingCard/BookingCard.js
@@ -13,9 +13,9 @@ const BookingCard = ({ booking, date }) => {
         <div className="col-md-4 mb-5">
             <div className="card p-3">
                 <div className="card-body text-center">
+                    <h5 className="card-title brand-color">{booking.exert}</h5>
                     <h5 className="card-title brand-color">{booking.subject}</h5>
                     <h6>{booking.visitingHour}</h6>
-                    <p>{booking.totalSpace} SPACES AVAILABLE</p>
                     <button onClick={openModal} className="btn btn-primary text-uppercase">Book Appointment</button>
                     <AppointmentForm modalIsOpen={modalIsOpen} closeModal={closeModal} appointmentOn={booking.subject} date={date}></AppointmentForm>
                 </div>
