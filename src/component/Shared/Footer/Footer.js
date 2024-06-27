5a7a5f8acf61b5200b3b3264d8fb4e7458f57e70
Juhi Bajpai <juhi.altid@gmail.com>
2024-06-27 20:20:18 +0530

    Update Footer.js

--- src/component/Shared/Footer/Footer.js
+++ src/component/Shared/Footer/Footer.js
@@ -3,11 +3,8 @@ import FooterDetail from '../FooterDetail/FooterDetail';
 
 const Footer = () => {
     const noNamed = [
-        {name: "Road Skills School" , link: "/road"},
-        {name: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"},
-        {img: "logo"},
-
-       
+        {name: "Psych-Friend" , link: "/road"},
+             
     ]
     const ourAddress = [
         {name: "New York - 101010 Hudson" , link: "//google.com/map"},
@@ -24,26 +21,21 @@ const Footer = () => {
         {name: "Check Up" , link: "/checkup"}
     ]
     const services = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"}
+        {name: "Dashboard" , link: "/dashboard"},
+        {name: "Appointment" , link: "/appointment"},
+        {name: "Login" , link: "/login"},
     ]
     return (
         <footer className="footer-area clear-both footer-bg-color">
             <div className="container pt-5">
                 <div className="row py-5">
-                    <FooterDetail key={1} menuTitle={"Road Skills School"} menuItems={noNamed}/>
-                    <FooterDetail key={2} menuTitle="Company" menuItems={services}/>
-                    <FooterDetail key={3} menuTitle="Oral Health" menuItems={oralHealth}/>
+                    <FooterDetail key={1} menuTitle={"Psych-Friend"} menuItems={noNamed}/>
+                    <FooterDetail key={2} menuTitle="Links" menuItems={services}/>
                     <FooterDetail key={4} menuTitle="Contact" menuItems={ourAddress}> 
                         <ul className="social-media list-inline">
-                            <li className="list-inline-item"><a href="//facebook.com">facebook</a></li>
-                            <li className="list-inline-item"><a href="//google.com"></a>Google</li>
-                            <li className="list-inline-item"><a href="//instagram.com"></a>Instragram</li>
+                            <li className="list-inline-item"><a href="//facebook.com">Facebook</a></li>
+                            <li className="list-inline-item"><a href="//google.com">Google</a></li>
+                            <li className="list-inline-item"><a href="//instagram.com">Instragram</a></li>
                         </ul>
                         <div className="mt-5">
                             <h6>Call now</h6>
