98f1985f13a75b5e2883b5d4ad9953080176fb81
Juhi Bajpai <juhi.altid@gmail.com>
2024-06-27 18:22:52 +0530

    Update Constructors.js

--- src/component/Home/Constructors/Constructors.js
+++ src/component/Home/Constructors/Constructors.js
@@ -13,9 +13,9 @@ const Constructors = () => {
     return (
         <div className="container services">
             <div className="service-desc text-center">
-                <h1 className="services-headline">Our <span className="brand-color">Expert Trainer</span></h1>
+                <h1 className="services-headline"><span className="brand-color">Our Experts</span></h1>
                 <div className="py-3">
-                    <p>The School offers the Following services for Teenages firs-time Drivers , new <br /> adult learnser and existing learners and existing drivers with lapsed licences.</p>
+                    
                 </div>
             </div>
             <hr />
